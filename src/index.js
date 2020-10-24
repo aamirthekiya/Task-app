161332280d9335fdf83913a56b491d36b7a65d88
Aamir Thekiya <thekiyaaamir@gmail.com>
2020-10-24 12:06:08 +0530

    Added API End point information for user at index page

--- src/index.js
+++ src/index.js
@@ -10,6 +10,35 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
+app.get("", (req, res) => {
+  res.send({
+    message: "This app contain API end points",
+    "GET Request API": {
+      "Read user": "/users/me",
+      "Read tasks":
+        "/tasks?  -- /sortBy=createdAt:desc, limit=2 etc optional parameters",
+      "Read task by id": "/tasks/task_id",
+    },
+    "POST Request API": {
+      "Create new user": "/users",
+      "Login user": "/users/login",
+      "Logout user": "/users/logout",
+      "Logout user from all devices": "/users/logoutAll",
+      "Upload profile pic": "/users/me/avatar",
+      "Create new task": "/tasks",
+    },
+    "PATCH Request API": {
+      "Update user": "/users/me",
+      "Update task": "/tasks/task_id",
+    },
+    "DELETE Request API": {
+      "Delete user profile pic": "/users/me/avatar",
+      "Delete user": "/users/me",
+      "Delete task": "/tasks/task_id",
+    },
+  });
+});
+
 app.listen(port, () => {
   console.log("Server is up on port: ", port);
 });
